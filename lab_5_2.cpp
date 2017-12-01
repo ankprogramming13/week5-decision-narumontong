f01d3439463833175cbd817431789b380882263b
Unknown <tongnarumon1998@gmail.com>
2017-12-01 14:14:30 +0700

    ข้อที่2

--- lab_5_2.cpp
+++ lab_5_2.cpp
@@ -15,7 +15,7 @@ int main() {
     if (name1.length() > 10) {
         cout << "Your name is too long!!!!" << endl;
         name1.erase (name1.begin()+10, name1.end());
-        cout << "Your name has been changed to " << name1 << endl;
+        } else {cout << "Your name has been changed to " << name1 << endl;
     }
 
     return 0;
